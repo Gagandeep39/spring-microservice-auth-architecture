124236ceddcaf5fbc9097f8f61995fa9041ea31d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-09 07:26:33 +0000

    Created a password encoder

---
+++ auth-service/src/main/java/com/gagan/authservice/config/CustomPasswordEncoder.java
@@ -0,0 +1,14 @@
+package com.gagan.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPasswordEncoder {
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+  
+}
