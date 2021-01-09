02b36963e86f7c8f602549c3644309ea84b85406
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-09 06:00:56 +0000

    Added hello world controller

---
+++ auth-service/src/main/java/com/gagan/authservice/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.gagan.authservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorld {
+
+  @GetMapping
+  public String helloWorld() {
+    return "Hello World";
+  }
+  
+}
