7a01ba6c27f0865c62f67c86c04de100a3ef5742
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-09 07:37:46 +0000

    Added authorization filter

--- auth-service/src/main/java/com/gagan/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/gagan/authservice/config/SecurityConfig.java
@@ -7,10 +7,10 @@
  */
 package com.gagan.authservice.config;
 
-import com.gagan.authservice.security.JwtAuthenticationFilter;
+import com.gagan.authservice.security.JwtAuthorizationFilter;
+import com.gagan.authservice.security.JwtProvider;
 import com.gagan.authservice.services.implementation.JwtUserDetailsServiceImpl;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,8 +18,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -29,28 +27,27 @@ import lombok.AllArgsConstructor;
 public class SecurityConfig extends WebSecurityConfigurerAdapter  {
   
   private JwtUserDetailsServiceImpl userDetailsService;
+  private BCryptPasswordEncoder passwordEncoder;
+  private JwtProvider jwtProvider;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
       .cors().and() // Required for accessing prpotected routes
       .csrf().disable()
-      // .authorizeRequests().antMatchers("/auth/**", "/h2/**", "/swagger*/**", "/v2/api-docs").permitAll()
-      // .antMatchers().permitAll()
-      // .anyRequest().authenticated()
-      // .and()
+      .authorizeRequests().antMatchers("/auth/**", "/h2/**", "/swagger*/**", "/v2/api-docs").permitAll()
+      .antMatchers().permitAll()
+      .anyRequest().authenticated()
+      .and()
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    http.addFilter(new JwtAuthenticationFilter(authenticationManager()));
+    // http.addFilter(new JwtAuthenticationFilter(authenticationManager()));
+    http.addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtProvider, userDetailsService));
     http.headers().frameOptions().disable();
   }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-  
+ 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
   }
 }
