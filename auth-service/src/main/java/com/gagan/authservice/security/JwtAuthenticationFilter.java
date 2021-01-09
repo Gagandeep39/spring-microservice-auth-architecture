b28f510ae4bfcf79f9177e6837404021529ea7f5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-09 07:26:54 +0000

    Created authentication filter

--- auth-service/src/main/java/com/gagan/authservice/security/JwtAuthenticationFilter.java
+++ auth-service/src/main/java/com/gagan/authservice/security/JwtAuthenticationFilter.java
@@ -1,9 +1,18 @@
 package com.gagan.authservice.security;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gagan.authservice.entity.User;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -13,15 +22,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+  private AuthenticationManager authenticationManager;
+
   public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+    this.authenticationManager = authenticationManager;
     setFilterProcessesUrl("/auth/login");
   }
-
 
   @Override
   public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
       throws AuthenticationException {
+    try {
+      User creds = new ObjectMapper().readValue(request.getInputStream(), User.class);
+      return authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(creds.getUsername(), creds.getPassword(), new ArrayList<>()));
+    } catch (Exception e) {
+      log.info(e.getMessage());
+    }
     return super.attemptAuthentication(request, response);
   }
-  
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+      Authentication authResult) throws IOException, ServletException {
+
+    super.successfulAuthentication(request, response, chain, authResult);
+  }
+
 }
