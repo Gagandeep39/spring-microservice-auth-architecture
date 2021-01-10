f296b29556959f6b820bc5731aab49e77963bcb0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-10 11:50:36 +0530

    Added jwt

---
+++ gateway-server/src/main/java/com/gagan/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.gagan.gatewayserver.config;
+
+import com.gagan.gatewayserver.security.CustomAuthenticationEntryPoint;
+import com.gagan.gatewayserver.security.JwtAuthorizationFilter;
+import com.gagan.gatewayserver.security.JwtProvider;
+import com.gagan.gatewayserver.services.implementation.JwtUserDetailsServiceImpl;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+  
+  private JwtUserDetailsServiceImpl userDetailsService;
+  private BCryptPasswordEncoder passwordEncoder;
+  private JwtProvider jwtProvider;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .cors().and() // Required for accessing prpotected routes
+      .csrf().disable()
+      .authorizeRequests().antMatchers("/auth-service/**" , "/h2/**", "/swagger*/**", "/v2/api-docs").permitAll()
+      .antMatchers().permitAll()
+      .anyRequest().authenticated()
+      .and()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and().exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
+    // http.addFilter(new JwtAuthenticationFilter(authenticationManager(), authService));
+    http.addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtProvider, userDetailsService));
+    http.headers().frameOptions().disable();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+  }
+
+  public AuthenticationEntryPoint authenticationEntryPoint() {
+    return new CustomAuthenticationEntryPoint();
+  }
+}
