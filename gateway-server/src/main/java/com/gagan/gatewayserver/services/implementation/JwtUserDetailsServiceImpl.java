3a30dd0d750d63e9995fc055ff890258233515e1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-10 05:50:44 +0000

    Added exception login in gateway

---
+++ gateway-server/src/main/java/com/gagan/gatewayserver/services/implementation/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.gagan.gatewayserver.services.implementation;
+
+import java.util.ArrayList;
+
+import com.gagan.gatewayserver.entity.User;
+import com.gagan.gatewayserver.exceptions.InvalidCredentialException;
+import com.gagan.gatewayserver.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository
+      .findByUsername(username)
+      .orElseThrow(() -> new InvalidCredentialException("username", "User " + username + " doesn't exist"));
+    return new org.springframework.security.core.userdetails.User(
+      user.getUsername(), 
+      user.getPassword(), 
+      new ArrayList<>()
+    );
+	}
+  
+}
