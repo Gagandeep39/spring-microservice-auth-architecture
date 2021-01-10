e3ee2b7b9a20ad8858bc6d87454714c4c33121a3
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-10 15:11:38 +0530

    Removed web config

--- gateway-server/src/main/java/com/gagan/gatewayserver/config/WebConfig.java
+++ gateway-server/src/main/java/com/gagan/gatewayserver/config/WebConfig.java
@@ -11,13 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry corsRegistry) {
-    corsRegistry
-      .addMapping("/**")
-      .allowedOrigins("*")
-      // .allowedMethods("*")
-      .maxAge(3600L)
-      .allowedHeaders("*")
-      // .exposedHeaders("Authorization")
-      .allowCredentials(true);
+    // corsRegistry
+    //   .addMapping("/**")
+    //   .allowedOrigins("*")
+    //   .allowedMethods("*")
+    //   .maxAge(3600L)
+    //   .allowedHeaders("*")
+    //   // .exposedHeaders("Authorization")
+      // .allowCredentials(true);
   }
 }
