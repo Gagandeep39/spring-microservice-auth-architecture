f56f9213198b90c9f6f7bb8a4f6304d3a3170268
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-10 13:44:51 +0530

    Fixed gatewar server

--- gateway-server/src/main/java/com/gagan/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/gagan/gatewayserver/GatewayServerApplication.java
@@ -2,8 +2,10 @@ package com.gagan.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
