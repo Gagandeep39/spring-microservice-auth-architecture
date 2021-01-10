2d03b8f68888719e15c358437c40251185644ef4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-10 22:50:47 +0530

    Removed unused code

--- gateway-server/src/main/java/com/gagan/gatewayserver/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/gagan/gatewayserver/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.gagan.gatewayserver.config;
 
-import java.util.Arrays;
-
-import com.gagan.gatewayserver.security.CorsFilter;
 import com.gagan.gatewayserver.security.CustomAuthenticationEntryPoint;
 import com.gagan.gatewayserver.security.JwtAuthorizationFilter;
 import com.gagan.gatewayserver.security.JwtProvider;
 import com.gagan.gatewayserver.services.implementation.JwtUserDetailsServiceImpl;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,10 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.session.SessionManagementFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.AllArgsConstructor;
 
@@ -33,29 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
   private BCryptPasswordEncoder passwordEncoder;
   private JwtProvider jwtProvider;
 
-  // @Bean
-  // CorsFilter corsFilter() {
-  //     CorsFilter filter = new CorsFilter();
-  //     return filter;
-  // }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-      CorsConfiguration configuration = new CorsConfiguration();
-      configuration.setAllowedOrigins(Arrays.asList("*, *"));
-      configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-      configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "x-auth-token"));
-      // configuration.setAllowCredentials(true);
-      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      source.registerCorsConfiguration("/**", configuration);
-      return source;
-  }
-
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-    // .addFilterBefore(corsFilter(), SessionManagementFilter.class)
-      // .cors().and()
       // Makes header as default with origin as *
       .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and() // Required for accessing prpotected routes
       .csrf().disable()
@@ -64,7 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
       .and()
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and().exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
-    // http.addFilter(new JwtAuthenticationFilter(authenticationManager(), authService));
     http.addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtProvider, userDetailsService));
     http.headers().frameOptions().disable();
   }
